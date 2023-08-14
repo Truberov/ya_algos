36c6e38ed22ab95e6bcdbf2e725acfa0c32ed82c
Trubberov <timofey.bokach.d@gmail.com>
2023-08-14 18:51:09 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -3,10 +3,13 @@ def main():
     len_arr = len(arr)
     last_elem = len_arr - 1
     for i in range(len_arr):
+        max_elem = arr[0], 0
         j = 0
-        while j < last_elem:
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        while j <= last_elem:
+            if arr[j] > max_elem[0]:
+                max_elem = arr[j], j
+            if j == last_elem:
+                arr[max_elem[1]], arr[last_elem] = arr[last_elem], arr[max_elem[1]]
             j += 1
         last_elem -= 1
     print(arr)
